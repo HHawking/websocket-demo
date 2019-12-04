fede4f55f3b1e7cfdf1ae9a1506d29ca166958a1
hh <hhawking@qq.com>
2019-12-04 20:50:34 +0800

    1

--- src/main/java/com/hhawking/websocket/demo/server/WebsocketServer.java
+++ src/main/java/com/hhawking/websocket/demo/server/WebsocketServer.java
@@ -1,6 +1,7 @@
 package com.hhawking.websocket.demo.server;
 
 import com.hhawking.websocket.demo.servicec.WebsocketService;
+import com.hhawking.websocket.demo.thread.StatisticsThread;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,12 +22,15 @@ import java.util.Set;
 @Component
 public class WebsocketServer {
 
-    //在线用户
-    private static final Map<Integer,Session> sessionMap = new HashMap<>();
-    //推送统计数的线程
-    private static final Map<Integer,StatisticsThread> threadMap = new HashMap<>();
-    //推送订阅车辆信息的线程
-    private static final Map<Integer,FollowThread> followThreadMap = new HashMap<>();
+    /**
+     * 在线用户
+     */
+    private static final Map<Integer, Session> SESSION_MAP = new HashMap<>();
+    /**
+     * 推送统计数的线程
+     */
+    private static final Map<Integer, StatisticsThread> THREAD_MAP = new HashMap<>();
+
 
     //注入service
     private static WebsocketService websocketService;
@@ -36,106 +40,95 @@ public class WebsocketServer {
     }
 
     @OnOpen
-    public synchronized void onOpen(@PathParam("userId") Integer userId, Session session){
+    public synchronized void onOpen(@PathParam("userId") Integer userId, Session session) {
         System.out.println("---------------用户" + userId + "请求连接---------");
-        addUser(userId,session);
-        System.out.println("用户" + userId + "连接成功，当前人数: " + sessionMap.size() + "列表线程数: " + threadMap.size() + " , 订阅线程数: " + followThreadMap.size());
+        addUser(userId, session);
+        System.out.println("用户" + userId + "连接成功，当前人数: " + SESSION_MAP.size());
 
     }
 
     @OnClose
-    public synchronized void onClose(@PathParam("userId") Integer userId,CloseReason reason){
+    public synchronized void onClose(@PathParam("userId") Integer userId, CloseReason reason) {
         System.out.println("---------------用户" + userId + "请求断开---------");
-        removeUser(userId,reason);
-        System.out.println("用户"+userId+"断开连接,剩余人数:"+sessionMap.size()  + "列表线程数: " + threadMap.size() + " , 订阅线程数: " + followThreadMap.size());
+        removeUser(userId, reason);
+        System.out.println("用户" + userId + "断开连接,剩余人数:" + SESSION_MAP.size());
     }
 
     /**
      * 链接错误执行
      */
     @OnError
-    public synchronized void onError(@PathParam("userId") Integer userId,Throwable error){
-        removeUser(userId,new CloseReason(CloseReason.CloseCodes.NO_EXTENSION, "客户端异常"));
-        System.err.println("用户 "+userId+" 连接时发生了错误 : "+error.getMessage());
+    public synchronized void onError(@PathParam("userId") Integer userId, Throwable error) {
+        removeUser(userId, new CloseReason(CloseReason.CloseCodes.NO_EXTENSION, "客户端异常"));
+        System.err.println("用户 " + userId + " 连接时发生了错误 : " + error.getMessage());
     }
 
     /**
      * 收到订阅请求
      */
     @OnMessage
-    public synchronized void onMessage(@PathParam("userId") Integer userId, String ids){
-        FollowThread oldThread = followThreadMap.get(userId);
-        if (null!=oldThread)
-            oldThread.exit=true;
-        followThreadMap.remove(userId);
-        FollowThread followThread = new FollowThread(userId, ids);
-        Thread thread = new Thread(followThread);
-        thread.start();
-        followThreadMap.put(userId,followThread);
-        System.out.println("用户"+userId+"订阅成功，当前订阅线程数"+ followThreadMap.size());
+    public synchronized void onMessage(@PathParam("userId") Integer userId, String msg) {
+        websocketService.setMsg(userId,msg);
     }
 
     /**
      * 发送信息(String)给指定ID用户
      */
-    private synchronized void sendToUser(String message,Integer sendUserId){
-        if(sessionMap.size()<=0)
+    private synchronized void sendToUser(String message, Integer sendUserId) {
+        if (SESSION_MAP.size() <= 0) {
             return;
-        Session session = sessionMap.get(sendUserId);
+        }
+        Session session = SESSION_MAP.get(sendUserId);
         try {
             session.getBasicRemote().sendText(message);
         } catch (Exception e) {
-            System.err.println("websocket IO异常"+e.getMessage());
+            System.err.println("websocket IO异常" + e.getMessage());
         }
     }
 
     /**
      * 发送信息给所有人
      */
-    private synchronized void sendToAll(String message)  {
-        if(sessionMap.size() == 0)
+    private synchronized void sendToAll(String message) {
+        if (SESSION_MAP.size() == 0) {
             return;
-
-        Set<Integer> set = sessionMap.keySet();
-        for(Integer key : set){
+        }
+        Set<Integer> set = SESSION_MAP.keySet();
+        for (Integer key : set) {
             try {
-                sessionMap.get(key).getBasicRemote().sendText(message);
+                SESSION_MAP.get(key).getBasicRemote().sendText(message);
             } catch (Exception e) {
-                System.err.println("websocket 异常,用户" + key +"发送失败 : "+e.getMessage());
+                System.err.println("websocket 异常,用户" + key + "发送失败 : " + e.getMessage());
             }
         }
     }
 
     /**
-     * 用户上线,开启实时数据推送线程
+     * 用户上线,开启消息推送线程
      */
-    private synchronized void addUser(Integer id, Session session){
+    private synchronized void addUser(Integer id, Session session) {
 
-        StatisticsThread oldThread = threadMap.get(id);
-        if (null != oldThread)
+        StatisticsThread oldThread = THREAD_MAP.get(id);
+        if (null != oldThread) {
             oldThread.exit = true;
-        threadMap.remove(id);
+        }
+        THREAD_MAP.remove(id);
 
-        FollowThread oldFollowThread = followThreadMap.get(id);
-        if (oldFollowThread!=null)
-            oldFollowThread.exit = true;
-        followThreadMap.remove(id);
-
-        Session oldSession = sessionMap.get(id);
+        Session oldSession = SESSION_MAP.get(id);
         if (null != oldSession) {
             try {
                 oldSession.close();
             } catch (IOException e) {
-                System.err.println("websocket IO异常"+e.getMessage());
+                System.err.println("websocket IO异常" + e.getMessage());
             }
         }
-        sessionMap.remove(id);
-        sessionMap.put(id,session);
-
-        StatisticsThread statisticsThread = new StatisticsThread(id);
+        SESSION_MAP.remove(id);
+        SESSION_MAP.put(id, session);
+        websocketService.setMsg(id,id.toString());
+        StatisticsThread statisticsThread = new StatisticsThread(websocketService,id);
         Thread thread = new Thread(statisticsThread);
         thread.start();
-        threadMap.put(id,statisticsThread);
+        THREAD_MAP.put(id, statisticsThread);
     }
 
 
@@ -143,81 +136,20 @@ public class WebsocketServer {
      * 用户下线
      */
     private synchronized void removeUser(Integer userId, CloseReason reason) {
-        StatisticsThread thread = threadMap.get(userId);
-        FollowThread followThread = followThreadMap.get(userId);
-        if (thread!=null)
+        StatisticsThread thread = THREAD_MAP.get(userId);
+        if (thread != null) {
             thread.exit = true;
-        if (followThread!=null)
-            followThread.exit = true;
-        threadMap.remove(userId);
-        followThreadMap.remove(userId);
-
-        Session session = sessionMap.get(userId);
-
-        if (session!=null) {
+        }
+        THREAD_MAP.remove(userId);
+        Session session = SESSION_MAP.get(userId);
+        if (session != null) {
             try {
                 session.close(reason);
             } catch (IOException e) {
-                System.err.println("websocket 异常 : "+e.getMessage());
+                System.err.println("websocket 异常 : " + e.getMessage());
             }
         }
-
-        sessionMap.remove(userId);
-    }
-
-    /**
-     * 每5秒推送实时统计的数据
-     */
-    private class StatisticsThread implements Runnable{
-        private volatile boolean exit = false;
-        private Integer id;
-        //传入用户id，用于查询他对应的数据
-        private StatisticsThread(Integer id) {
-            this.id = id;
-        }
-
-        @Override
-        public void run() {
-            while (!exit) {
-
-                sendToUser("实时统计的数据", id);
-
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
-
-    /**
-     * 每5秒推送一次订阅的车辆信息
-     */
-    private class FollowThread implements Runnable{
-        private volatile boolean exit = false;
-        private Integer id;
-        private String ids;
-        //传入用户的id,和需要订阅的车辆id
-        FollowThread(Integer id, String ids) {
-            this.id = id;
-            this.ids = ids;
-        }
-        @Override
-        public void run() {
-            while (!exit){
-
-                sendToUser("订阅的车辆:"+ids+"的信息",id);
-
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
+        SESSION_MAP.remove(userId);
     }
 
 }
--- src/main/java/com/hhawking/websocket/demo/servicec/WebsocketService.java
+++ src/main/java/com/hhawking/websocket/demo/servicec/WebsocketService.java
@@ -2,6 +2,19 @@ package com.hhawking.websocket.demo.servicec;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class WebsocketService {
+
+    Map<Integer,String> map = new HashMap<>();
+
+    public String getMsg(Integer id) {
+        return map.get(id);
+    }
+
+    public void setMsg(Integer id,String msg) {
+        map.put(id,msg);
+    }
 }
